31face1fd4cbaff834279ee2183e9ccea86683d8
Aaron Pinero <aaronpinero@gmail.com>
2022-01-23 22:09:21 -0500

    fonts to match other personal work; remove fontawesome

--- icons.js
+++ icons.js
@@ -47,17 +47,6 @@ for (x=0;x<iconsdata.bold.length;x++) {
   // newcode += '    <div class="card-footer text-center"><code>.'+iconsdata.bold[x].class+'</code></div>\n  </div>\n</div>\n\n';
   icons_bold_code.push(newcode);
 }
-for (x=0;x<iconsdata.fa.length;x++) {
-  var newcode = '<!-- ICON CARD -->\n';
-  newcode += '<div class="col-6 col-md-4 col-lg-3 col-xl-2 mb-3">\n  <div class="card">\n';
-  newcode += '    <div class="card-body text-center">\n';
-  newcode += '      <div><span class="fs-3 '+iconsdata.fa[x].class+'"></span></div>\n';
-  newcode += '      <div class="mt-2 text-muted">'+iconsdata.fa[x].name+'</div>\n';
-  newcode += '    </div>\n';
-  newcode += '</div>\n</div>\n\n';
-  // newcode += '    <div class="card-footer text-center"><code>.'+iconsdata.bold[x].class+'</code></div>\n  </div>\n</div>\n\n';
-  icons_fa_code.push(newcode);
-}
 
 // pull in the page with the template
 var file = fs.readFileSync("./index-temp.html");
@@ -65,5 +54,4 @@ templatesjs.setSync(file);
 file = templatesjs.renderSync("icons_regular", icons_regular_code);
 file = templatesjs.renderSync("icons_light", icons_light_code);
 file = templatesjs.renderSync("icons_bold", icons_bold_code);
-file = templatesjs.renderSync("icons_fa", icons_fa_code);
 fs.writeFileSync("./index.html",file);

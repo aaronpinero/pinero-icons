2ce0266a1a7df2774d9dd053e4ed97f187782515
Aaron Pinero <aaronpinero@gmail.com>
2022-02-05 21:58:06 -0500

    updated and tested node packages

--- dist/picons-regular/react/External.js
+++ dist/picons-regular/react/External.js
@@ -2,5 +2,5 @@
 import React from 'react';
 
 export const External = props => (
-  <svg viewBox="0 0 20 20" {...props}><path d="M32,8 L32,14 L14,14 L14,50 L50,50 L50,32 L56,32 L56,56 L8,56 L8,8 L32,8 Z M64,0 L64,24 L58,24 L58,12 L58.593,9.649 L29.1213203,39.1213203 L24.8786797,34.8786797 L54.335,5.421 L52,6 L40,6 L40,0 L64,0 Z" fillRule="evenodd" /></svg>
+  <svg viewBox="0 0 20 20" {...props}><path d="M32,8 L32,14 L14,14 L14,50 L50,50 L50,32 L56,32 L56,56 L8,56 L8,8 L32,8 Z M64,0 L64,24 L58,24 L58,12 L58.593,9.649 L29.121,39.121 L24.878,34.878 L54.335,5.421 L52,6 L40,6 L40,0 L64,0 Z" fillRule="evenodd" /></svg>
 );
